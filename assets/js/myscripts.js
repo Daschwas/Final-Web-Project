e70e2cb2d095c9e88be9c5ce54850c7f028c2c6e
Daschwas <schwass.dale@gmail.com>
2023-11-27 19:08:33 +0800

    feat: adding commentary

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,9 +1,13 @@
+// Import the Collapse module from the 'flowbite' package
+
 import { Collapse } from '../../node_modules/flowbite';
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Get references to the navbar and navbar toggle elements
     const $navbar = document.getElementById('navbar-target');
     const $navbarToggle = document.getElementById('navbar-trigger');
 
+    // Check if both the navbar and navbar toggle elements exist
     if ($navbar && $navbarToggle) {
         const options = {
             onCollapse: () => {
@@ -22,8 +26,12 @@ document.addEventListener('DOMContentLoaded', function () {
             override: true,
         };
 
+        // Create a new instance of Collapse with the specified options
         const navbarCollapse = new Collapse($navbar, $navbarToggle, options, instanceOptions);
-    } else {
+    }
+
+    // Log an error if either the navbar or navbar toggle elements are not found
+    else {
         console.error('Navbar elements not found. Check your HTML structure and element IDs.');
     }
 });
--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,13 +1,17 @@
+// This method opens a specific tab by displaying only the chosen one
 function openTab(tabName) {
     let i;
     let tabContent;
 
+    // Get all elements with the class "tab-content"
     tabContent = document.getElementsByClassName("tab-content");
 
+    // Hide all tab content elements
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
 
+    // Display the selected tab
     document.getElementById(tabName).style.display = "flex";
 }
 
@@ -15,6 +19,7 @@ let designLinkEl = document.getElementById("Project1Link");
 let progLinkEl = document.getElementById("Project2Link");
 let musicLinkEl = document.getElementById("Project3Link");
 
+// Event listeners to each project link, calling openTab for the relevant ProjectTab
 designLinkEl.addEventListener("click", function(){openTab("Project1")}, false);
 progLinkEl.addEventListener("click", function(){openTab("Project2")}, false);
 musicLinkEl.addEventListener("click", function(){openTab("Project3")}, false);
